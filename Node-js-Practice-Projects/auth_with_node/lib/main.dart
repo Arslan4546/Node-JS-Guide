3d2e429fd1687c25ea9d967df1f62a1f92715f2e
Arslan4546 <arslantariq4546@gmail.com>
2026-04-27 17:46:19 +0500

    update

--- Node-js-Practice-Projects/auth_with_node/lib/main.dart
+++ Node-js-Practice-Projects/auth_with_node/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      showPerformanceOverlay: true,
       debugShowCheckedModeBanner: false,
       initialRoute: '/login',
       routes: {
